9d03432803c9ec5aac514c2a634359cd1736f24a
Josie Christensen <josiexensen@gmail.com>
2022-09-10 10:01:28 -0400

    sets screen size for all Actors instead of individually

--- characters/actor.gd
+++ characters/actor.gd
@@ -2,7 +2,9 @@ extends KinematicBody2D
 class_name Actor
 
 # everything that moves has... velocity, speed, and uses the same screen size
-var velocity = Vector2.ZERO
-export var speed = 100
+var velocity:= Vector2.ZERO
+export var speed:= 100
 var screen_size
 
+func _ready():
+	screen_size = get_viewport_rect().size
