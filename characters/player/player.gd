4f5a862f61c91f2ac9d6b074a1d533077dafb88e
Josie Christensen <josiexensen@gmail.com>
2022-09-10 10:11:20 -0400

    added an idle timer. replaced hard-coded node references with $onreadys. started implementation of stats. added tile focus collisions for using tools on tiles #TODO move to tools when tools are implemented

--- characters/player/player.gd
+++ characters/player/player.gd
@@ -1,70 +1,79 @@
 extends Actor
 
 # sprite animations
-const SPRITES = {"REST": "player_rest", "LEFT": "player_left", "RIGHT": "player_right", 
+const SPRITES:= {"REST": "player_rest", "LEFT": "player_left", "RIGHT": "player_right", 
 	"UP": "player_up", "DOWN": "player_down", 
 	"FACING_UP": "player_facing_up", "FACING_DOWN": "player_facing_down", 
 	"FACING_LEFT": "player_facing_left", "FACING_RIGHT": "player_facing_right"}
-# idle animation delay (in frames)
-export var DELAY_AMOUNT = 240
-var delay = DELAY_AMOUNT
+
+onready var idle_timer: Timer = $IdleTimer
+onready var tile_focus: CollisionShape2D = $focus_area/tile_focus
+onready var player: AnimatedSprite = $player
+# player stats and data (for easy save and load)
+var stats: character_data setget set_stats
+var player_inventory: inventory setget set_inventory
+
 
 # starts player on it's idle animation
-func _ready():
-	$player.animation = "player_rest"
-	screen_size = get_viewport_rect().size
+func _ready() -> void:
+	player.animation = "player_rest"
+	set_stats(preload("res://systems/save_and_load/character_data.tres"))
 
 # player movement
-func _process(delta: float) -> void:	
+func _physics_process(_delta: float) -> void:
 	velocity.x = 0
 	velocity.y = 0
 	setMovementDirection()
 	setFacingDirection()
-	idleAnimationDelay()
 	if velocity.length() > 0:
-		velocity = velocity.normalized() * speed
-	$player.play()
+		velocity = velocity.normalized() * stats.run_speed
+	player.play()
 	velocity = move_and_slide(velocity)
 	position.x = clamp(position.x, 0, screen_size.x)
 	position.y = clamp(position.y, 0, screen_size.y)
-
-# resets the delay timer
-func resetDelay():
-	delay = DELAY_AMOUNT
 	
-# sets velocity and animation while in motion, resets the idle delay timer
-func setMovementDirection():
+# sets velocity and animation while in motion
+# resets the idle delay timer
+# sets facing direction for tile focus
+func setMovementDirection() -> void:
+	var input_vector: Vector2 = Vector2.ZERO
+	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
 	if Input.is_action_pressed("ui_right"):
-		velocity.x += 1
-		$player.animation = SPRITES["RIGHT"]
-		resetDelay()
+		player.animation = SPRITES["RIGHT"]
+		idle_timer.start()
+		tile_focus.direction = tile_focus.directions.RIGHT
 	elif Input.is_action_pressed("ui_left"):
-		velocity.x -= 1
-		$player.animation = SPRITES["LEFT"]
-		resetDelay()
+		player.animation = SPRITES["LEFT"]
+		idle_timer.start()
+		tile_focus.direction = tile_focus.directions.LEFT
 	elif Input.is_action_pressed("ui_down"):
-		velocity.y += 1
-		$player.animation = SPRITES["DOWN"]
-		resetDelay()
+		player.animation = SPRITES["DOWN"]
+		idle_timer.start()
+		tile_focus.direction = tile_focus.directions.DOWN
 	elif Input.is_action_pressed("ui_up"):
-		velocity.y -= 1
-		$player.animation = SPRITES["UP"]
-		resetDelay()
+		player.animation = SPRITES["UP"]
+		idle_timer.start()
+		tile_focus.direction = tile_focus.directions.UP
+	velocity = input_vector
 
-# sets facing direction while stopped
-func setFacingDirection():
+# sets sprite facing direction while stopped
+func setFacingDirection() -> void:
 	if Input.is_action_just_released("ui_right"):
-		$player.animation = SPRITES["FACING_RIGHT"]
+		player.animation = SPRITES["FACING_RIGHT"]
 	elif Input.is_action_just_released("ui_left"):
-		$player.animation = SPRITES["FACING_LEFT"]
+		player.animation = SPRITES["FACING_LEFT"]
 	elif Input.is_action_just_released("ui_up"):
-		$player.animation = SPRITES["FACING_UP"]
+		player.animation = SPRITES["FACING_UP"]
 	elif Input.is_action_just_released("ui_down"):
-		$player.animation = SPRITES["FACING_DOWN"]
+		player.animation = SPRITES["FACING_DOWN"]
 
-# starts the idle animation after the specified delay (reset by moving)
-func idleAnimationDelay():
-	delay -= 1
-	if delay <= 0:
-		$player.animation = SPRITES["REST"]
+# plays idle animation
+func _on_IdleTimer_timeout() -> void:
+	player.animation = SPRITES["REST"]
+	
+func set_stats(new_stats: character_data) -> void:
+	stats = new_stats
 
+func set_inventory(new_inventory: inventory) -> void:
+	player_inventory = new_inventory 
